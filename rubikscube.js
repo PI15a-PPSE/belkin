90fb748614b8210f30049766d04e82c9f758baa5
balanar451 <belckindimon@gmail.com>
2018-11-14 09:13:24 +0300

    Wrote some code into create function

--- rubikscube.js
+++ rubikscube.js
@@ -105,6 +105,38 @@ cubeApp.init = function(){
 * This function creates the rubiks cube and all the necessary groups to make it move/rotate
 */
 cubeApp.createRubiks = function(){
+ 	//Create geometry
+	var geom = new THREE.CubeGeometry(2, 2, 2); 
+	// Setup cube face color for each face
+	var index = 0;
+	for(var i = 0; i < this.cubeColors.length; i ++){
+		//triangles so need to do twice for each face
+		geom.faces[index].color = this.cubeColors[i]
+		geom.faces[index+1].color = this.cubeColors[i]
+		index+=2
+	}
+	this.rubiksCube = new THREE.Object3D();
+	//Create material for cube
+	var cubeMaterial = new THREE.MeshPhongMaterial({ vertexColors: THREE.FaceColors});
+	cubeMaterial.normalMap = this.normalMap;
+	var yPos = this.startY;
+	// Triple for-loop to create all 27 individual cubes that make up the rubiks cube
+	for(var y = 0; y < 3; y ++){
+		var zPos = this.startZ;
+		for(var z = 0; z < 3 ; z ++){
+			var xPos = this.startX;
+			for(var x = 0; x < 3; x ++){
+				var cube = new THREE.Mesh(geom, cubeMaterial); //Give the cube the geometry and material
+			    cube.position.z = zPos;
+			    cube.position.x = xPos;
+			    cube.position.y = yPos;
+			    this.rubiksCube.add(cube);	
+			    xPos += this.SPACING;
+			}
+			zPos += this.SPACING;
+		}
+		yPos += this.SPACING;
+	}
 
 }
 
